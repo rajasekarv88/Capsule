ee0aa76cf5c0840d5ea81cb42226cc8a3f2f3456
Admin <Admin@DOTNET>
2018-11-27 17:49:03 +0530

    TaskManger

--- Capsule_TaskManager/App_Start/WebApiConfig.cs
+++ Capsule_TaskManager/App_Start/WebApiConfig.cs
@@ -13,12 +13,17 @@ namespace Capsule_TaskManager
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.EnableCors();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //config.Routes.MapHttpRoute(
+            //name: "API Default",
+            //routeTemplate: "api/{controller}/{action}/{id}",
+            //defaults: new { id = RouteParameter.Optional });
         }
     }
 }
